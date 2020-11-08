6d9eb931fd46274dbc16bdf91a400502cd609c68
DJ-Yang <dong3831@hanmail.net>
2020-11-08 18:46:23 +0900

    token

--- eduproject/settings.py
+++ eduproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'instagram',
     'accounts',
 ]
@@ -125,6 +126,10 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
     'PAGE_SIZE': 3,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSTION_CLASSES': [
